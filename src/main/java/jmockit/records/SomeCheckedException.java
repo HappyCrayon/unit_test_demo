e179837fb2a344225913b9ba485ea88903f0be7e
Yonggao <ygdong@thoughtworks.com>
2017-11-30 09:10:28 +0800

    既然名字叫 UncheckedException, 那当初为什么继承 RuntimeException?

--- src/main/java/jmockit/records/SomeCheckedException.java
+++ src/main/java/jmockit/records/SomeCheckedException.java
@@ -3,5 +3,5 @@ package jmockit.records;
 /**
  * Created by Yonggao.Dong on 16/9/14.
  */
-public class SomeCheckedException extends RuntimeException{
+public class SomeCheckedException extends Exception{
 }
